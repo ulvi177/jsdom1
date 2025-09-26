83d54b10c79928b1e29c4744bbf4fd6466dfc864
ulvi177 <ulvi-abdullayev@internet.ru>
2025-09-26 17:28:31 +0000

    jsdom

--- app.js
+++ app.js
@@ -1,4 +1,45 @@
-"use strict";
+
 $(document).ready(function () {
+
+    $("#me").click(function (e) {
+        e.preventDefault()
+        function myel(myclass = "") {
+            if (!$.trim($("#inputme").val()) == "") {
+                let vall = $("#inputme").val()
+                    , newH3 = $('<h3> </h3>')
+                    , newDiv = $('<div class="line"></div>')
+                    , bigDiv = $(`<div ${myclass}'> </div>`)
+                $(newH3).append(vall)
+                $(newDiv).append(newH3)
+                $(bigDiv).append(newDiv)
+                return bigDiv
+            }
+            else {
+                return 
+            }
+
+        }
+        $("#messageme").append(myel('class="end"'))
+        $("#inputme").val("")  
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
