0ae5278ade516c021360b44872a68d574b557fa9
ulvi177 <ulvi-abdullayev@internet.ru>
2025-10-10 12:56:14 +0000

    jsdoom

--- app.js
+++ app.js
@@ -19,12 +19,33 @@ $(document).ready(function () {
             }
 
         }
-        $("#messageyou").append(myel())
         $("#messageme").append(myel('class="end"'))
         $("#inputme").val("")  
+        document.getElementById('messageme').scrollTop = document.getElementById('messageme').scrollHeight; 
     });
 
+    $("#you").click(function (e) {
+        e.preventDefault()
+        function myel(myclass = "") {
+            if (!$.trim($("#inputyou").val()) == "") {
+                let vall = $("#inputyou").val()
+                    , newH3 = $('<h3> </h3>')
+                    , newDiv = $('<div class="line"></div>')
+                    , bigDiv = $(`<div ${myclass}'> </div>`)
+                $(newH3).append(vall)
+                $(newDiv).append(newH3)
+                $(bigDiv).append(newDiv)
+                return bigDiv
+            }
+            else {
+                return 
+            }
 
+        }
+        $("#messageyou").append(myel('class="end"'))
+        $("#inputyou").val("")  
+        document.getElementById('messageyou').scrollTop = document.getElementById('messageyou').scrollHeight;   
+    });
 
 
 
